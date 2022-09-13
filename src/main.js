df6aed384956b81ee01cc26199d146e830eada6d
Ivan <ivandanilov282@gmail.com>
2022-09-13 13:18:56 +0200

    Axios call

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import bootstrap from './assets/scss/app.scss'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,54 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home container">
+    <div>
+      <table class="table py-5 my-5">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">User Name</th>
+            <th scope="col">Roles</th>
+            <th scope="col">Submit</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="i in users.data.data">
+            <th scope="row">{{ i.id }}</th>
+            <td>{{ i.name }}</td>
+            <td>
+              <input type="checkbox" id="first" name="name1" value="Admin">
+              <label for="first">Admin</label><br/>
+              <input type="checkbox" id="second" name="name1" value="Superadmin">
+              <label for="second">Superadmin</label><br/>
+              <input type="checkbox" id="third" name="name1" value="Moderator">
+              <label for="third">Moderator</label><br/>
+              <input type="checkbox" id="fourth" name="name1" value="Basic">
+              <label for="fourth">Basic</label>
+            </td>
+            <td>
+              <button class="btn btn-primary">Submit</button></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import axios from "axios";
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+      users: null,
+    }
+  },
+  mounted () {
+    axios.get('http://127.0.0.1:8000/api/users').then(response => (this.users = response))
   }
 }
 </script>
